2b596c97b0108bc5454cc5ada596b1c9651a5830
crawkatt <man849916@gmail.com>
2024-10-11 16:27:07 -0300

    Reformat CSS code and add typewriter effect

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function showSection(sectionId) {
     sections.forEach((section) => {
-      sectionId === "all" || section.id === sectionId
-        ? section.classList.remove("hidden", "hide")
-        : (() => {
-            section.classList.add("hidden");
-            setTimeout(() => {
-              section.classList.add("hide");
-            }, 550);
-          })();
+      if (sectionId === "all" || section.id === sectionId) {
+        section.classList.remove("hidden", "hide");
+      } else {
+        section.classList.add("hidden");
+        setTimeout(() => {
+          section.classList.add("hide");
+        }, 550);
+      }
     });
   }
 
@@ -19,63 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
-      const sectionId = event.target.getAttribute("data-section");
+      const sectionId = this.getAttribute("data-section");
       showSection(sectionId);
 
-      navLinks.forEach((link) => link.classList.remove("active"));
-      event.target.classList.add("active");
+      navLinks.forEach((btn) => btn.classList.remove("active"));
+      this.classList.add("active");
     });
   });
 
   showSection("all");
 });
-
-/*
-  // TypeWriter
-
-  // Animación del título
-  const h1Text =
-    "Modder de Minecraft, Programador en Rust y Administrador de Sistemas";
-
-  const h1Element = document.getElementById("typing-h1");
-
-  const typingSpeed = 100;
-  const erasingSpeed = 50;
-  const delayBetweenTexts = 2000;
-  const delayBetweenLoops = 1000;
-
-  function typeText(element, text, index, callback) {
-    if (index < text.length) {
-      element.textContent += text.charAt(index);
-      setTimeout(() => {
-        typeText(element, text, index + 1, callback);
-      }, typingSpeed);
-    } else {
-      setTimeout(callback, delayBetweenTexts);
-    }
-  }
-
-  function eraseText(element, callback) {
-    const text = element.textContent;
-    if (text.length > 0) {
-      element.textContent = text.substring(0, text.length - 1);
-      setTimeout(() => {
-        eraseText(element, callback);
-      }, erasingSpeed);
-    } else {
-      setTimeout(callback, delayBetweenLoops);
-    }
-  }
-
-  function loopTyping() {
-    typeText(h1Element, h1Text, 0, () => {
-      eraseText(h1Element, () => {
-        loopTyping();
-      });
-    });
-  }
-
-  // Iniciar la animación
-  loopTyping();
-});
-*/
