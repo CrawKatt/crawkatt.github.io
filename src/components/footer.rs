1771b02b75bbfb2284008ba7632d4baeb74d4184
CrawKatt <man849916@gmail.com>
2025-04-11 22:22:11 -0400

    Agrega Términos y Condiciones al Footer

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -7,7 +7,7 @@ pub fn Footer() -> impl IntoView {
         <footer class="border-t py-6">
             <div class="container flex flex-col items-center justify-center gap-4 md:flex-row md:justify-between px-4 sm:px-6">
                 <p class="text-center text-sm leading-loose text-muted-foreground md:text-left">
-                    {"© 2025"}{" CrawKatt. All rights reserved."}
+                    "© 2025 CrawKatt. All rights reserved."
                 </p>
                 <div class="flex items-center gap-4">
                     <a
@@ -29,6 +29,12 @@ pub fn Footer() -> impl IntoView {
                             <MailIcon />
                         </div>
                         <span class="sr-only">"Email"</span>
+                    </a>
+                    <a
+                        href="/terms-and-conditions"
+                        class="text-center text-sm leading-loose text-muted-foreground md:text-right hover:text-primary"
+                    >
+                        "Términos y Condiciones."
                     </a>
                 </div>
             </div>
