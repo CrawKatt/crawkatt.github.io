7937b4b4218393158b2a0dd16fb2e490c2478373
CrawKatt <man849916@gmail.com>
2025-06-12 19:41:44 -0400

    🐛 Fix: Arregla el modo claro/oscuro en terms_and_conditions.rs

--- src/pages/oss_contributions.rs
+++ src/pages/oss_contributions.rs
@@ -30,6 +30,24 @@ pub fn OSSContributions() -> impl IntoView {
             .pr_url("https://github.com/RustLangES/rust-book-es/pull/2")
             .stars(96)
             .merged(true)
+            .build(),
+
+        Contribution::builder()
+            .title("Traducción del Mod UltimateCraft de BassStarling")
+            .description("Añadiendo traducción al idioma Español")
+            .repo_url("https://github.com/BassStarling/ultimatecraft-1.21")
+            .pr_url("https://github.com/BassStarling/ultimatecraft-1.21/pull/2")
+            .stars(0)
+            .merged(true)
+            .build(),
+
+        Contribution::builder()
+            .title("Ancient Times Mod Contribution")
+            .description("Fix bugs and add features to Ancient Times Mod")
+            .repo_url("https://github.com/DeluAxolol/Ancient-Times")
+            .pr_url("https://github.com/DeluAxolol/Ancient-Times/pull/1")
+            .stars(2)
+            .merged(true)
             .build()
     ];
 
