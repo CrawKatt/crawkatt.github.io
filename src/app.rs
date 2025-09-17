dbaf05a1f45af808ba61f8ee4afcd3b9f4dacec6
CrawKatt <man849916@gmail.com>
2025-09-17 13:36:01 -0300

    ⚰️ remove: Remueve líneas de código innecesarias

--- src/app.rs
+++ src/app.rs
@@ -20,31 +20,21 @@ fn I18nProvider(children: Children) -> impl IntoView {
         cookie_attrs: "SameSite=Strict; Secure; path=/; max-age=600",
         initial_language_from_cookie: true,
         initial_language_from_cookie_to_local_storage: true,
-        initial_language_from_cookie_to_server_function: set_language_server_function,
         set_language_to_cookie: true,
-        url_param: "lang",
-        initial_language_from_url_param: true,
         initial_language_from_url_param_to_local_storage: true,
         initial_language_from_url_param_to_cookie: true,
-        set_language_to_url_param: true,
         local_storage_key: "language",
         initial_language_from_local_storage: true,
         initial_language_from_local_storage_to_cookie: true,
-        initial_language_from_local_storage_to_server_function: set_language_server_function,
         set_language_to_local_storage: true,
         initial_language_from_navigator: true,
         initial_language_from_navigator_to_cookie: true,
-        initial_language_from_navigator_to_server_function: set_language_server_function,
         initial_language_from_accept_language_header: true,
         initial_language_from_server_function: initial_language_server_function,
         initial_language_from_server_function_to_cookie: true,
         initial_language_from_server_function_to_local_storage: true,
-        set_language_to_server_function: set_language_server_function,
-        url_path: get_language_from_url_path,
-        initial_language_from_url_path: true,
         initial_language_from_url_path_to_cookie: true,
         initial_language_from_url_path_to_local_storage: true,
-        initial_language_from_url_path_to_server_function: set_language_server_function,
     }
 }
 
@@ -81,33 +71,5 @@ pub fn App() -> impl IntoView {
 #[server(InitialLanguage, "/api")]
 pub async fn initial_language_server_function(
 ) -> Result<Option<String>, ServerFnError> {
-    // .. replace with your own logic
     Ok(Some("es".to_string()))
-}
-
-#[server(ShowHelloWorld, "/api")]
-pub async fn show_hello_world(
-    translated_hello_world: String,
-    language: String,
-) -> Result<(), ServerFnError> {
-    #[allow(clippy::print_stdout)]
-    {
-        println!("{translated_hello_world} ({language})");
-    };
-    Ok(())
-}
-
-#[server(SetLanguage, "/api")]
-pub async fn set_language_server_function(
-    _language: String,
-) -> Result<(), ServerFnError> {
-    // .. replace with your own logic
-    Ok(())
-}
-
-fn get_language_from_url_path(path: &str) -> &str {
-    if let Some(language) = path.split('/').nth(1) {
-        return language;
-    }
-    ""
 }
